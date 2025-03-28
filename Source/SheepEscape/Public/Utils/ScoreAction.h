c65dc8ab694a628a4a0a4df8e9095d3fbbd18713
Thibault Clodion <thibaultclodion@gmail.com>
2025-03-28 10:08:18 +0100

    Add Score mechanics (only in data)
    J'ai encore besoin de faire en sorte que lorsque l'on effectue une action, cela l'ajoute à la game instance

---
+++ Source/SheepEscape/Public/Utils/ScoreAction.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum EScoreAction : uint8
+{
+	ESA_SheepElimateSheep UMETA(DisplayName = "Sheep eliminate other sheep"),
+	ESA_SheepElimateSheperd UMETA(DisplayName = "Sheep eliminate shepherd"),
+	ESA_SheepNotIdentify  UMETA(DisplayName = "Sheep not identify"),
+	ESA_ShepherdRescueSheep UMETA(DisplayName = "Shepherd rescue a sheep"),
+	ESA_ShepherdEliminateFakeSheep UMETA(DisplayName = "Shepherd eliminate a fake sheep")
+};
